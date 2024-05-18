9277184e36a79e82dfe78f13d2d105a3d9681e5b
Maxim Thibault <e2038653@cmaisonneuve.qc.ca>
2024-05-18 18:46:20 -0400

    Correction pour avoir une liste de tuples

--- Exercice 1.py
+++ Exercice 1.py
@@ -1,11 +1,11 @@
 import json
 
-data = {
-    2:3,
-    3:2,
-    1.0 : -5.3,
-    0.7 : 13
-}
+data = [
+    [2,3],
+    [3,2],
+    [1.0,-5.3],
+    [0.7, 13]
+]
 
 with open('data.json', 'w') as json_file:
-    json.dump(data, json_file, indent=4,)
+    json.dump(data, json_file,)
